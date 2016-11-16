4b6404d45851f0f9f93695f3c104b4255be6a09f
yazid2j <yazid2j@gmail.com>
2016-11-16 11:02:26 +0100

    test Google Maps

---
+++ Maps/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Geocoding service</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+</head>
+<body>
+<div class="container">
+<form action="app/events/Add.php" method="post">
+	<label>Nom de l'évènement</label>	
+	<input  type="textbox" name="nomEvent"><br>
+	<label>Latitude</label>	
+	<input id="Latitude" type="textbox" name="lat"><br>
+	<label>Longitude</label>	
+	<input id="Longitude" type="textbox" name="lng"><br>
+    <input id="Ajouter" type="submit" value="Ajouter un évènement">
+</form>
+<div>
+	<label>Nom de la ville</label>
+	<input id="address" type="textbox" >
+    <input id="submit" type="Button" value="Montrer la ville">
+</div>
+
+<div id="map"></div>
+</div>
+<script>
+    function initMap() {
+    	
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 5,
+            center: {lat: 39.753768, lng: 10.644}
+        });
+        var geocoder = new google.maps.Geocoder();
+
+        document.getElementById('submit').addEventListener('click', function() {
+            geocodeAddress(geocoder, map);
+        });
+
+	 	var marker = new google.maps.Marker({
+	    	position: {lat: 39.753768, lng: 10.644},
+	    	map: map,
+	    	title: "bougez moi",
+	    	draggable: true,
+	    });
+
+	 	google.maps.event.addListener(marker, 'drag', function(){
+	 		setPosition(marker);
+
+
+	 	});
+
+
+    }
+
+    
+    function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                resultsMap.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                    map: resultsMap,
+                    position: results[0].geometry.location
+                });
+                
+            } else {
+                alert('La ville est introuvable: ' + status);
+            }
+        });
+    }
+    
+    function setPosition(marker) {
+    	var pos= marker.getPosition();
+
+		$('#Latitude').val(pos.lat());
+		$('#Longitude').val(pos.lng());
+		
+	}
+
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCyJ5_W_BaAsUAhJff4Ewrq7P845mJ1Udg&callback=initMap"
+        async defer></script>
+
+</body>
+</html>
